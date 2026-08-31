ca56644e6e0024dfee3b206c0222b18c17687bee
MJ-Ahmad <mjahmad@gmail.com>
2026-09-01 03:20:31 +0600

    Updates

--- _next/static/208A5_j21SNdcCW0qbiUU/_buildManifest.js
+++ _next/static/208A5_j21SNdcCW0qbiUU/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/mjs//_next/:path+",
+        "destination": "/mjs/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
